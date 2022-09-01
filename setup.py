0719cd5e2a478febdcf2c64499d4ab503e78c66e
GitYCC <ycc.tw.email@gmail.com>
2022-09-01 18:09:15 +0800

    [config] Bump to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='g2pw',
-    version='0.1.0',
+    version='0.1.1',
     license='Apache License 2.0',
     author='Yi-Chang Chen',
     author_email='ycc.tw.email@gmail.com',
